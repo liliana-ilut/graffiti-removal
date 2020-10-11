7c7d6feb0c8f94500596e87620be4dbc7c7e266b
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-10-11 18:25:25 -0500

    updates

--- static/layers2.js
+++ static/layers2.js
@@ -6,15 +6,12 @@
 function createMap(graffitiSpot) {
 
     // Create the tile layer that will be the background of our map
-    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/streets-v11",
+        id: "light-v10",
         accessToken: API_KEY
-      }).addTo(myMap);
-    
+      });
     
     // Create a baseMaps object to hold the lightmap layer
     var baseMaps = {
@@ -29,7 +26,7 @@ function createMap(graffitiSpot) {
     // Create the map object with options
     var myMap = L.map("map", {
       center: [41.87, -87.62],
-      zoom: 13,
+      zoom: 12,
       layers: [lightmap, graffitiSpot]
     });
   
