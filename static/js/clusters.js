2f52ab29191dac8ef2a1cd21014e08a8db380bc2
kayoung7766 <65790635+kayoung7766@users.noreply.github.com>
2020-10-14 14:49:11 -0500

    reuploading

--- static/js/clusters.js
+++ static/js/clusters.js
@@ -1,51 +1,52 @@
 var myMap = L.map("map", {
-    center: [41.87, -87.62],
-    zoom: 13
-  });
+  center: [41.87, -87.62],
+  zoom: 13
+});
+
+// Adding tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Store API query variables
+
+var url = "/api"
+// Grab the data with d3
+d3.json(url).then(function(response) {
+
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup();
+
+  // Loop through data
+  for (var i = 0; i < response.length; i++) {
+    
+    
+
+    // Set the data location property to a variable
+    var lat = response[i].latitude;
+    var lng = response[i].longitude;
+   
+
+    // Check for location property
+    
+      var locationString = "Latitude: " + lat + ", Longitude:" + lng;
+      
   
-  // Adding tile layer to the map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
-  
-  // Store API query variables
-  var baseURL = "https://data.cityofchicago.org/resource/hec5-y4x5.json?";
-  var date = "$where=creation_date between '2018-11-06T00:00:00.000' and '2018-12-18T00:00:00.000'";
-  var status = "&status=Completed";
-  var limit = "&$limit=10000";
-  
-  // Assemble API query URL
-  var url = baseURL + date + status + limit;
-  
-  // Grab the data with d3
-  d3.json(url).then(function(response) {
-  
-    // Create a new marker cluster group
-    var markers = L.markerClusterGroup();
-  
-    // Loop through data
-    for (var i = 0; i < response.length; i++) {
-  
-      // Set the data location property to a variable
-      var location = response[i].location;
-  
-      // Check for location property
-      if (location) {
-  
-        // Add a new marker to the cluster group and bind a pop-up
-        markers.addLayer(L.marker([location.latitude, location.longitude])
-          .bindPopup(response[i].descriptor));
-      }
-  
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([lat, lng])
+        .bindPopup(locationString));
+      //console.log(response[i].location)
     }
+
   
-    // Add our marker cluster layer to the map
-    myMap.addLayer(markers);
-  
-  });
-  
+
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
+
+});
--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,40 +1,41 @@
 
 var myMap = L.map("map", {
-    center: [41.87, -87.62],
-    zoom: 13
-  });
+  center: [41.87, -87.62],
+  zoom: 13
+});
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+
+
+let url= "/api";
+d3.json(url).then(function(response) {
+
+  console.log(response);
+
+  var heatArray = [];
+
+  for (var i = 0; i < response.length; i++) {
+    var lat = response[i].latitude;
+    var lng = response[i].longitude;
+
+    
+      heatArray.push([lat, lng]);
   
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
+  }
+  console.log(heatArray);
+  var heat = L.heatLayer(heatArray, {
+    radius: 20,
+    blur: 35
   }).addTo(myMap);
 
-  
-  
-  let url= "https://data.cityofchicago.org/resource/hec5-y4x5.json?$limit=10000";
-  d3.json(url).then(function(response) {
-  
-    console.log(response);
-  
-    var heatArray = [];
-  
-    for (var i = 0; i < response.length; i++) {
-      var location = response[i].location;
-  
-      if (location) {
-        heatArray.push([location.latitude, location.longitude]);
-      }
-    }
-    console.log(heatArray);
-    var heat = L.heatLayer(heatArray, {
-      radius: 20,
-      blur: 35
-    }).addTo(myMap);
 
-  
-   
-  });
+ 
+});
